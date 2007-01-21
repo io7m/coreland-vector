98808d45b1e73a7cfaf2fc67b8b3f7b10ca63327
markzero_svn <markzero_svn@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-21 15:47:27 +0000

    vec_zero SSE2
    git-svn-id: file:///source/coreland/vector/trunk@1587 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- vec_zero.c
+++ vec_zero.c
@@ -30,8 +30,25 @@ static float *vec_zeroNf_sse(float *va, unsigned int ne)
 #endif
 
 #ifdef SYS_HAVE_CPU_EXT_SSE2
-static double *vec_zeroNd_sse2(double *va, unsigned int n)
+static double *vec_zeroNd_sse2(double *va, unsigned int ne)
 {
+  __m128d mvz;
+  unsigned int seg[2];
+  double *pva;
+  unsigned int ind;
+
+  pva = va;
+  mvz = _mm_setzero_pd();
+  vec_segments(seg, 2, ne);
+
+  for (ind = 0; ind < seg[1]; ++ind) {
+    _mm_store_pd(pva, mvz);
+    _mm_store_pd(pva + 2, mvz);
+    pva += 4;
+  }
+  for (ind = 0; ind < seg[0]; ++ind)
+    pva[ind] = 0;
+
   return va;
 }
 #endif

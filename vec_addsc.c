9e25ba750e8565030f70eb9de5b2278ec0082bcf
markzero_svn <markzero_svn@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-21 15:16:58 +0000

    vec_add vec_addsc SSE2
    git-svn-id: file:///source/coreland/vector/trunk@1583 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- vec_addsc.c
+++ vec_addsc.c
@@ -80,11 +80,61 @@ static float *vec_addscNfx_sse(const float *va, float *vr, float sc,
 #ifdef SYS_HAVE_CPU_EXT_SSE2
 static double *vec_addscNd_sse2(double *va, double sc, unsigned int ne)
 {
+  __m128d mva1;
+  __m128d mva2;
+  __m128d vsc;
+  unsigned int seg[2];
+  double *pva;
+  unsigned int ind;
+
+  vsc = _mm_set1_pd(sc);
+  pva = va;
+  vec_segments(seg, 2, ne);
+
+  for (ind = 0; ind < seg[1]; ++ind) {
+    mva1 = _mm_load_pd(pva);
+    mva2 = _mm_load_pd(pva + 2);
+    mva1 = _mm_add_pd(mva1, vsc);
+    mva2 = _mm_add_pd(mva2, vsc);
+    _mm_store_pd(pva, mva1);
+    _mm_store_pd(pva + 2, mva2);
+    pva += 4;
+  }
+  for (ind = 0; ind < seg[0]; ++ind)
+    pva[ind] += sc;
+
   return va;
 }
 static double *vec_addscNdx_sse2(const double *va, double *vr, double sc,
                                  unsigned int ne)
 {
+  __m128d mva1;
+  __m128d mva2;
+  __m128d mvr;
+  __m128d vsc;
+  unsigned int seg[2];
+  const double *pva;
+  double *pvr;
+  unsigned int ind;
+
+  pva = va;
+  pvr = vr;
+  vsc = _mm_set1_pd(sc);
+  vec_segments(seg, 2, ne);
+
+  for (ind = 0; ind < seg[1]; ++ind) {
+    mva1 = _mm_load_pd(pva);
+    mva2 = _mm_load_pd(pva + 2);
+    mvr = _mm_add_pd(mva1, vsc);
+    _mm_store_pd(pvr, mvr);
+    mvr = _mm_add_pd(mva2, vsc);
+    _mm_store_pd(pvr + 2, mvr);
+    pva += 4;
+    pvr += 4;
+  }
+  for (ind = 0; ind < seg[0]; ++ind)
+    pvr[ind] = pva[ind] + sc;
+
   return vr;
 }
 #endif

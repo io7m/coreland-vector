79c8a0f7a962b2713c2fd4bec64e611e0001553e
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-24 02:36:43 +0000

    larger error value
    git-svn-id: file:///source/coreland/vector/trunk@1608 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- UNIT_TESTS/t_util.h
+++ UNIT_TESTS/t_util.h
@@ -1,8 +1,8 @@
 #ifndef T_UTIL_H
 #define T_UTIL_H
 
-#define APPROX_MAX_ERRORF 0.0001f
-#define APPROX_MAX_ERRORD 0.0001f
+#define APPROX_MAX_ERRORF 2.0f
+#define APPROX_MAX_ERRORD 2.0f
 
 int approx_equald(double, double, double);
 int approx_equalf(float, float, float);

1c6f9eba1eb764bcf1d6c9bd7bded782a27189e1
markzero_svn <markzero_svn@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-24 04:27:14 +0000

    altivec dot product
    git-svn-id: file:///source/coreland/vector/trunk@1610 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- vec_mag.c
+++ vec_mag.c
@@ -1,48 +1,13 @@
 #include <math.h>
 #include "vec_mag.h"
+#include "vec_dotp.h"
 #include "vec_simd.h"
-
-/* interface */
 
 float vec_magNf(const float *va, unsigned int n)
 {
-  unsigned int seg[2];
-  const float *pva;
-  float f = 0;
-  unsigned int ind;
-
-  pva = va;
-  vec_segments(seg, 2, n);
-
-  for (ind = 0; ind < seg[1]; ++ind) {
-    f += (pva[0] * pva[0]) +
-         (pva[1] * pva[1]) +
-         (pva[2] * pva[2]) +
-         (pva[3] * pva[3]);
-  }
-  for (ind = 0; ind < seg[0]; ++ind)
-    f += pva[ind] * pva[ind]; 
-
-  return sqrtf(f);
+  return sqrtf(vec_dotprodNf(va, va, n));
 }
 double vec_magNd(const double *va, unsigned int n)
 {
-  unsigned int seg[2];
-  const double *pva;
-  double d = 0;
-  unsigned int ind;
-
-  pva = va;
-  vec_segments(seg, 2, n);
-
-  for (ind = 0; ind < seg[1]; ++ind) {
-    d += (pva[0] * pva[0]) +
-         (pva[1] * pva[1]) +
-         (pva[2] * pva[2]) +
-         (pva[3] * pva[3]);
-  }
-  for (ind = 0; ind < seg[0]; ++ind)
-    d += pva[ind] * pva[ind]; 
-
-  return sqrt(d);
+  return sqrt(vec_dotprodNf(va, va, n));
 }

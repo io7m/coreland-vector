c64ed22460f824cfd63a86622020ef60eb605bbb
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-03-08 06:17:35 +0000

    speed test fixes
    git-svn-id: file:///source/coreland/vector/trunk@1865 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- SPEED_TESTS/t_norm2_sse.sh
+++ SPEED_TESTS/t_norm2_sse.sh
@@ -3,9 +3,9 @@
 . ./test-subs
 
 ${CC} ${CFLAGS} ${SSE_FLAGS} \
-  -o t_norm2_pure -DVECTOR_NO_SIMD t_norm2.c ../vec_norm.c ../vec_simd.c frand.c
+  -o t_norm2_pure -DVECTOR_NO_SIMD t_norm2.c ../vec_norm.c ../vec_dotp.c ../vec_multsc.c ../vec_simd.c frand.c
 ${CC} ${CFLAGS} ${SSE_FLAGS} \
-  -o t_norm2_sse t_norm2.c ../vec_norm.c ../vec_simd.c frand.c
+  -o t_norm2_sse t_norm2.c ../vec_norm.c ../vec_dotp.c ../vec_multsc.c ../vec_simd.c frand.c
 
 sse=`./t_norm2_sse`
 pure=`./t_norm2_pure`
--- SPEED_TESTS/t_norm4_sse2.sh
+++ SPEED_TESTS/t_norm4_sse2.sh
@@ -3,9 +3,9 @@
 . ./test-subs
 
 ${CC} ${CFLAGS} ${SSE_FLAGS} \
-  -o t_norm4_pure -DVECTOR_NO_SIMD t_norm4.c ../vec_norm.c ../vec_dotp.c ../vec_simd.c frand.c
+  -o t_norm4_pure -DVECTOR_NO_SIMD t_norm4.c ../vec_norm.c ../vec_dotp.c ../vec_multsc.c ../vec_simd.c frand.c
 ${CC} ${CFLAGS} ${SSE_FLAGS} \
-  -o t_norm4_sse2 t_norm4.c ../vec_norm.c ../vec_dotp.c ../vec_simd.c frand.c
+  -o t_norm4_sse2 t_norm4.c ../vec_norm.c ../vec_dotp.c ../vec_multsc.c ../vec_simd.c frand.c
 
 sse2=`./t_norm4_sse2`
 pure=`./t_norm4_pure`

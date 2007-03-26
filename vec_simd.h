55b8d19d4387be5d243677480ed23d195d2c7292
markzero_svn <markzero_svn@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-03-26 07:50:08 +0000

    vec_simd Sun C update
    git-svn-id: file:///source/coreland/vector/trunk@2010 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- vec_simd.h
+++ vec_simd.h
@@ -9,6 +9,15 @@
   #undef SYS_HAVE_CPU_EXT_ALTIVEC
 #endif
 
+/* this can be replaced with a compiler version check when Sun C
+   supports SSE correctly */
+
+#if SYS_CCTYPE == SYS_CCTYPE_SUN_C
+  #undef SYS_HAVE_CPU_EXT_SSE
+  #undef SYS_HAVE_CPU_EXT_SSE2
+  #undef SYS_HAVE_CPU_EXT_ALTIVEC
+#endif
+
 #ifdef SYS_HAVE_CPU_EXT_SSE
 #include <xmmintrin.h>
 #endif

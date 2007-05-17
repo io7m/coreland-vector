653da3cc1847c070a251c310a16698016c690300
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-05-17 07:30:11 +0000

    fixed logical error in macro
    git-svn-id: file:///source/coreland/vector/trunk@2315 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- vec_align.c
+++ vec_align.c
@@ -1,14 +1,14 @@
 #include "vec_align.h"
 #include "_sd_ptr_uint.h"
 
-#define IS_ALIGNED(p) (((ptr_uint) (p)) & 15)
+#define IS_UNALIGNED(p) (((ptr_uint) (p)) & 15)
 
 #if defined(VECTOR_VERBOSE_UNALIGNED)
 #include <stdio.h>
 
 int vec_unaligned_core(const char *func, unsigned long line, const void *vp)
 {
-  if (IS_ALIGNED(vp)) {
+  if (IS_UNALIGNED(vp)) {
     fprintf(stderr, "%s %lu: warn: %p unaligned vector access\n", func, line, vp);
     return 1;
   } else
@@ -17,6 +17,6 @@ int vec_unaligned_core(const char *func, unsigned long line, const void *vp)
 #else
 int vec_unaligned_core(const char *func, unsigned long line, const void *vp)
 {
-  return !IS_ALIGNED(vp);
+  return IS_UNALIGNED(vp);
 }
 #endif

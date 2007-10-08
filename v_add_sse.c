429083a80fa914f1c656707db593d16562a47eca
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-10-08 09:19:10 +0000

    attempt to use all SSE registers
    git-svn-id: file:///source/coreland/vector/trunk@2809 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- v_add_sse.c
+++ v_add_sse.c
@@ -8,17 +8,41 @@ static inline float
 {
   __m128 mva1;
   __m128 mva2;
+  __m128 mva3;
+  __m128 mva4;
   __m128 mvb1;
   __m128 mvb2;
-  unsigned int seg[3] = {0, 0, 0};
+  __m128 mvb3;
+  __m128 mvb4;
+  unsigned int seg[4] = {0, 0, 0, 0};
   const float *pvb;
   float *pva;
   unsigned int ind;
 
   pva = va;
   pvb = vb;
-  vec_segments(seg, 3, ne);
+  vec_segments(seg, 4, ne);
 
+  for (ind = 0; ind < seg[3]; ++ind) {
+    mva1 = _mm_load_ps(pva);
+    mva2 = _mm_load_ps(pva + 4);
+    mva3 = _mm_load_ps(pva + 8);
+    mva4 = _mm_load_ps(pva + 12);
+    mvb1 = _mm_load_ps(pvb);
+    mvb2 = _mm_load_ps(pvb + 4);
+    mvb3 = _mm_load_ps(pvb + 8);
+    mvb4 = _mm_load_ps(pvb + 12);
+    mva1 = _mm_add_ps(mva1, mvb1);
+    mva2 = _mm_add_ps(mva2, mvb2);
+    mva3 = _mm_add_ps(mva3, mvb3);
+    mva4 = _mm_add_ps(mva4, mvb4);
+    _mm_store_ps(pva, mva1);
+    _mm_store_ps(pva + 4, mva2);
+    _mm_store_ps(pva + 8, mva3);
+    _mm_store_ps(pva + 12, mva4);
+    pva += 16;
+    pvb += 16;
+  }
   for (ind = 0; ind < seg[2]; ++ind) {
     mva1 = _mm_load_ps(pva);
     mva2 = _mm_load_ps(pva + 4);
@@ -50,10 +74,14 @@ vec_addNfx_sse(const float *va, const float *vb, float *vr, unsigned int ne)
 {
   __m128 mva1;
   __m128 mva2;
+  __m128 mva3;
+  __m128 mva4;
   __m128 mvb1;
   __m128 mvb2;
+  __m128 mvb3;
+  __m128 mvb4;
   __m128 mvr;
-  unsigned int seg[3] = {0, 0, 0};
+  unsigned int seg[4] = {0, 0, 0, 0};
   const float *pva;
   const float *pvb;
   float *pvr;
@@ -62,8 +90,29 @@ vec_addNfx_sse(const float *va, const float *vb, float *vr, unsigned int ne)
   pva = va;
   pvb = vb;
   pvr = vr;
-  vec_segments(seg, 3, ne);
+  vec_segments(seg, 4, ne);
 
+  for (ind = 0; ind < seg[3]; ++ind) {
+    mva1 = _mm_load_ps(pva);
+    mva2 = _mm_load_ps(pva + 4);
+    mva3 = _mm_load_ps(pva + 8);
+    mva4 = _mm_load_ps(pva + 12);
+    mvb1 = _mm_load_ps(pvb);
+    mvb2 = _mm_load_ps(pvb + 4);
+    mvb3 = _mm_load_ps(pvb + 8);
+    mvb4 = _mm_load_ps(pvb + 12);
+    mvr = _mm_add_ps(mva1, mvb1);
+    _mm_store_ps(pvr, mvr);
+    mvr = _mm_add_ps(mva2, mvb2);
+    _mm_store_ps(pvr + 4, mvr);
+    mvr = _mm_add_ps(mva3, mvb3);
+    _mm_store_ps(pvr + 8, mvr);
+    mvr = _mm_add_ps(mva4, mvb4);
+    _mm_store_ps(pvr + 12, mvr);
+    pva += 16;
+    pvb += 16;
+    pvr += 16;
+  }
   for (ind = 0; ind < seg[2]; ++ind) {
     mva1 = _mm_load_ps(pva);
     mva2 = _mm_load_ps(pva + 4);

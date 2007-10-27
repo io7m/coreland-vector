f51bec2597bb97b60a69272429d55e6ec6f754dc
m0 <m0@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-10-27 23:24:33 +0000

    'optimize' sse
    git-svn-id: file:///source/coreland/vector/trunk@2823 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- v_sub_sse.c
+++ v_sub_sse.c
@@ -6,27 +6,45 @@ static inline float *
 vec_subNf_sse(float *va, const float *vb, unsigned int ne)
 {
   __m128 mva1;
-  __m128 mva2;
   __m128 mvb1;
-  __m128 mvb2;
-  unsigned int seg[3];
+  unsigned int seg[4];
   const float *pvb;
   float *pva;
   unsigned int ind;
 
   pva = va;
   pvb = vb;
-  vec_segments(seg, 3, ne);
+  vec_segments(seg, 4, ne);
 
+  for (ind = 0; ind < seg[3]; ++ind) {
+    mva1 = _mm_load_ps(pva);
+    mvb1 = _mm_load_ps(pvb);
+    mva1 = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pva, mva1);
+    mva1 = _mm_load_ps(pva + 4);
+    mvb1 = _mm_load_ps(pvb + 4);
+    mva1 = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pva + 4, mva1);
+    mva1 = _mm_load_ps(pva + 8);
+    mvb1 = _mm_load_ps(pvb + 8);
+    mva1 = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pva + 8, mva1);
+    mva1 = _mm_load_ps(pva + 12);
+    mvb1 = _mm_load_ps(pvb + 12);
+    mva1 = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pva + 12, mva1);
+    pva += 16;
+    pvb += 16;
+  }
   for (ind = 0; ind < seg[2]; ++ind) {
     mva1 = _mm_load_ps(pva);
-    mva2 = _mm_load_ps(pva + 4);
     mvb1 = _mm_load_ps(pvb);
-    mvb2 = _mm_load_ps(pvb + 4);
     mva1 = _mm_sub_ps(mva1, mvb1);
-    mva2 = _mm_sub_ps(mva2, mvb2);
     _mm_store_ps(pva, mva1);
-    _mm_store_ps(pva + 4, mva2);
+    mva1 = _mm_load_ps(pva + 4);
+    mvb1 = _mm_load_ps(pvb + 4);
+    mva1 = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pva + 4, mva1);
     pva += 8;
     pvb += 8;
   }
@@ -48,11 +66,9 @@ static inline float *
 vec_subNfx_sse(const float *va, const float *vb, float *vr, unsigned int ne)
 {
   __m128 mva1;
-  __m128 mva2;
   __m128 mvb1;
-  __m128 mvb2;
   __m128 mvr;
-  unsigned int seg[3];
+  unsigned int seg[4];
   const float *pva;
   const float *pvb;
   float *pvr;
@@ -61,16 +77,37 @@ vec_subNfx_sse(const float *va, const float *vb, float *vr, unsigned int ne)
   pva = va;
   pvb = vb;
   pvr = vr;
-  vec_segments(seg, 3, ne);
+  vec_segments(seg, 4, ne);
 
-  for (ind = 0; ind < seg[2]; ++ind) {
+  for (ind = 0; ind < seg[3]; ++ind) {
     mva1 = _mm_load_ps(pva);
-    mva2 = _mm_load_ps(pva + 4);
     mvb1 = _mm_load_ps(pvb);
-    mvb2 = _mm_load_ps(pvb + 4);
     mvr = _mm_sub_ps(mva1, mvb1);
     _mm_store_ps(pvr, mvr);
-    mvr = _mm_sub_ps(mva2, mvb2);
+    mva1 = _mm_load_ps(pva + 4);
+    mvb1 = _mm_load_ps(pvb + 4);
+    mvr = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pvr + 4, mvr);
+    mva1 = _mm_load_ps(pva + 8);
+    mvb1 = _mm_load_ps(pvb + 8);
+    mvr = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pvr + 8, mvr);
+    mva1 = _mm_load_ps(pva + 12);
+    mvb1 = _mm_load_ps(pvb + 12);
+    mvr = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pvr + 12, mvr);
+    pva += 16;
+    pvb += 16;
+    pvr += 16;
+  }
+  for (ind = 0; ind < seg[2]; ++ind) {
+    mva1 = _mm_load_ps(pva);
+    mvb1 = _mm_load_ps(pvb);
+    mvr = _mm_sub_ps(mva1, mvb1);
+    _mm_store_ps(pvr, mvr);
+    mva1 = _mm_load_ps(pva + 4);
+    mvb1 = _mm_load_ps(pvb + 4);
+    mvr = _mm_sub_ps(mva1, mvb1);
     _mm_store_ps(pvr + 4, mvr);
     pva += 8;
     pvb += 8;

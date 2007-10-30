5eea1278192f99b630c7472be9e7e94f2042c1ae
m0 <m0@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-10-30 13:21:40 +0000

    tuned zero
    git-svn-id: file:///source/coreland/vector/trunk@2838 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- v_zero_sse.c
+++ v_zero_sse.c
@@ -1,30 +1,59 @@
-#include "v_align.h"
 #include "v_zero.h"
+#include "v_align.h"
 #include "v_simd.h"
+#include "v_inline.h"
+
+static inline float *
+vec_zeroNf_sse_lt16(float *va, unsigned int ne)
+{
+  __m128 mvz; 
+  float *pva;
+  unsigned int ind;
+  unsigned int n4;
+  unsigned int nr;
+
+  mvz = _mm_setzero_ps();
+  pva = va;
+  n4 = ne >> 2;
+  nr = ne - (n4 << 2);
+
+  for (ind = 0; ind < n4; ++ind) {
+    _mm_store_ps(pva, mvz);
+    pva += 4;
+  }
+  for (ind = 0; ind < nr; ++ind)
+    pva[ind] = 0;
+
+  return va;
+}
+
+static inline float *
+vec_zeroNf_sse_gte16(float *va, unsigned int ne)
+{
+  __m128 mvz; 
+  float *pva;
+  unsigned int ind;
+  unsigned int n16;
+  unsigned int nr;
+
+  mvz = _mm_setzero_ps();
+  pva = va;
+  n16 = ne >> 4;
+  nr = ne - (n16 << 4);
+
+  for (ind = 0; ind < n16; ++ind) {
+    _mm_store_ps(pva, mvz); pva += 4;
+    _mm_store_ps(pva, mvz); pva += 4;
+    _mm_store_ps(pva, mvz); pva += 4;
+    _mm_store_ps(pva, mvz); pva += 4;
+  }
+
+  vec_zeroNf_sse_lt16(pva, nr);
+  return va;
+}
 
 static inline float *
 vec_zeroNf_sse(float *va, unsigned int ne)
 {
-  __m128 mvz;
-  unsigned int seg[3];
-  float *pva;
-  unsigned int ind;
-
-  pva = va;
-  vec_segments(seg, 3, ne);
-
-  mvz = _mm_setzero_ps();
-  for (ind = 0; ind < seg[2]; ++ind) {
-    _mm_store_ps(pva, mvz);
-    _mm_store_ps(pva + 4, mvz);
-    pva += 8;
-  }
-  for (ind = 0; ind < seg[1]; ++ind) {
-    _mm_store_ps(pva, mvz);
-    pva += 4;
-  }
-  for (ind = 0; ind < seg[0]; ++ind)
-    pva[ind] = 0;
-
-  return va;
+  return (ne >= 16) ? vec_zeroNf_sse_gte16(va, ne) : vec_zeroNf_sse_lt16(va, ne);
 }

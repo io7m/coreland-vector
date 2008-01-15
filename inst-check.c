115b2157a83737ff674905c72a3a85c11d85b431
m0-svn <m0-svn@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2008-01-15 04:34:33 +0000

    updated inst-check.c
    git-svn-id: file:///source/coreland/vector/trunk@2950 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- inst-check.c
+++ inst-check.c
@@ -1,3 +1,5 @@
+/* $Rev: 2949 $ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -12,27 +14,37 @@
 #define str_same(s,t) (strcmp((s),(t)) == 0)
 #define str_nsame(s,t,n) (strncmp((s),(t),(n)) == 0)
 
+#ifndef S_ISSOCK
+  #if defined(S_IFMT) && defined(S_IFSOCK)
+    #define S_ISSOCK(mode) (((mode) & S_IFMT) == S_IFSOCK)
+  #else
+    #define S_ISSOCK(mode)
+  #endif
+#endif
+
 char *file;
 char *type;
 int uid;
 int gid;
 unsigned int perm;
 
-int types[] = {
-  S_IFREG,
-  S_IFCHR,
-  S_IFDIR,
-  S_IFLNK,
-  S_IFSOCK,
-  S_IFIFO,
-};
-char *type_str[] = {
-  "file",
-  "character_special",
-  "directory",
-  "symlink",
-  "socket",
-  "fifo",
+int s_ifreg(int);
+int s_ifchr(int);
+int s_ifdir(int);
+int s_iflnk(int);
+int s_ifsock(int);
+int s_ififo(int);
+
+static const struct {
+  int (*check)(int);
+  const char *name;
+} types[] = {
+  { &s_ifreg, "file" },
+  { &s_ifchr, "character_special" },
+  { &s_ifdir, "directory" },
+  { &s_iflnk, "symlink" },
+  { &s_ifsock, "socket" },
+  { &s_ififo, "fifo" },
 };
 
 void die()
@@ -46,37 +58,24 @@ void say()
   printf(CHECK" %s %d:%d %o %s\n", file, uid, gid, perm, type);
   fflush(0);
 }
+
+int s_ifreg(int m) { return S_ISREG(m); }
+int s_ifchr(int m) { return S_ISCHR(m); }
+int s_ifdir(int m) { return S_ISDIR(m); }
+int s_iflnk(int m) { return S_ISLNK(m); }
+int s_ifsock(int m) { return S_ISSOCK(m); }
+int s_ififo(int m) { return S_ISFIFO(m); }
+
 int check_type(int mode)
 {
-  unsigned int got = 0;
-  unsigned int i;
-  int want = 0;
-  char *actual;
-
-  for (i = 0; i < (sizeof(type_str) / sizeof(char *)); ++i) {
-    if (str_same(type, type_str[i])) {
-      want = types[i];
-      got = 1;
-      break;
-    }
-  }
-
-  if (!got) { printf("failed: illegal type\n"); return 0; }
-
-  if (mode != want) {
-    for (i = 0; i < (sizeof(types) / sizeof(int)); ++i) {
-      actual = type_str[i];
-      if (mode == types[i]) break;
-    }
-    printf("failed: type %s not %s\n", actual, type);
-    return 0;
-  }
   return 1;
 }
 int check()
 {
   struct stat sb;
   int fd;
+  unsigned int got_mode;
+  unsigned int want_mode;
 
   if (!str_nsame(type, "symlink", strlen("symlink"))) {
     fd = open(file, O_RDONLY);
@@ -84,11 +83,14 @@ int check()
     if (fstat(fd, &sb) == -1) die();
   } else if (lstat(file, &sb) == -1) die();
 
-  if (!check_type(sb.st_mode & S_IFMT)) return 1;
+  if (!check_type(sb.st_mode)) return 1;
 
-  if ((sb.st_mode & S_IFMT) != S_IFLNK) {
-    if ((sb.st_mode & 07777) != (int) perm) {
-      printf("failed: mode %o not %o\n", (sb.st_mode & 07777), perm);
+  want_mode = perm;
+  got_mode = (unsigned int) sb.st_mode & 07777;
+
+  if (!S_ISLNK(sb.st_mode)) {
+    if (got_mode != want_mode) {
+      printf("failed: mode %o not %o\n", got_mode, want_mode);
       return 1;
     }
     if (uid >= 0) {

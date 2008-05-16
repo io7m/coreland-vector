712d2c3e94ca23a0117adc382b4d2f75991a193e
m0 <m0@localhost>
2008-05-16 21:45:56 +0000

    added ifdef for SSE3

--- v_dotp_sse3.c
+++ v_dotp_sse3.c
@@ -3,9 +3,13 @@
 #include "v_types.h"
 #include "v_simd.h"
 
+#ifdef HAVE_CPU_EXT_SSE3
+
 static inline double
 vec_dotprodNd_sse3(const double *va, const double *vb, unsigned int ne)
 {
   float res;
   return res;
 }
+
+#endif

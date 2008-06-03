775a848146da0da90555a8f15a95d96669d54590
m0 <m0@localhost>
2008-06-03 09:01:58 +0100

    vec_dist implementation. untested.

--- v_dist.c
+++ v_dist.c
@@ -3,42 +3,18 @@
 #include "v_align.h"
 #include "v_inline.h"
 
+#include "v_mag.h"
 #include "v_dist.h"
+#include "v_sub.h"
 
-/*
-float vec_dist2f (const float va[2], const float vb[2])
+float
+vec_distNf (float *vt, const float *va, const float *vb, unsigned int n)
 {
-  float vc[2];
-  return vec_mag2f (vec_sub2fx (va, vb, vc));
+  return vec_magNf (vec_subNfx (va, vb, vt, n), n);
 }
 
-float vec_dist3f (const float va[3], const float vb[3])
+double
+vec_distNd (double *vt, const double *va, const double *vb, unsigned int n)
 {
-  float vc[3];
-  return vec_mag3f (vec_sub3fx (va, vb, vc));
+  return vec_magNd (vec_subNdx (va, vb, vt, n), n);
 }
-
-float vec_dist4f (const float va[4], const float vb[4])
-{
-  float vc[4];
-  return vec_mag4f (vec_sub4fx (va, vb, vc));
-}
-
-double vec_dist2d (const double va[2], const double vb[2])
-{
-  double vc[2];
-  return vec_mag2d (vec_sub2dx (va, vb, vc));
-}
-
-double vec_dist3d (const double va[3], const double vb[3])
-{
-  double vc[3];
-  return vec_mag3d (vec_sub3dx (va, vb, vc));
-}
-
-double vec_dist4d (const double va[4], const double vb[4])
-{
-  double vc[4];
-  return vec_mag4d (vec_sub4dx (va, vb, vc));
-}
-*/

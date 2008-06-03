0dc797d3f121e59811ad90f25b47f33bf217cf4a
m0 <m0@localhost>
2008-06-03 09:02:13 +0100

    todo and minor formatting update.

--- v_angle.c
+++ v_angle.c
@@ -12,12 +12,12 @@ float
 vec_angleNf (const float *va, const float *vb, unsigned int n)
 {
   return acosf (vec_dotprodNf (va, vb, n)
-               / ((vec_magNf (va, n) * vec_magNf (vb, n))));
+    / ((vec_magNf (va, n) * vec_magNf (vb, n))));
 }
 
 double
 vec_angleNd (const double *va, const double *vb, unsigned int n)
 {
   return acos (vec_dotprodNd (va, vb, n)
-              / ((vec_magNd (va, n) * vec_magNd (vb, n))));
+    / ((vec_magNd (va, n) * vec_magNd (vb, n))));
 }

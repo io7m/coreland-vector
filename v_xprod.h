f0e313b210438a0885362375c15109959e5466e8
m0 <m0@localhost>
2008-06-07 15:18:10 +0100

    fixed cross product mistake.

--- v_xprod.h
+++ v_xprod.h
@@ -6,9 +6,9 @@ double *vec_crossprod3dx (const double [3], const double [3], double [3]);
 
 #define vec_CROSSPROD3x(va,vb,vr)                      \
 {                                                      \
-  (vr)[0] = ((va)[1] * (vb)[2]) - ((va)[2] - (vb)[1]); \
-  (vr)[1] = ((va)[2] * (vb)[0]) - ((va)[0] - (vb)[2]); \
-  (vr)[2] = ((va)[0] * (vb)[1]) - ((va)[1] - (vb)[0]); \
+  (vr)[0] = ((va)[1] * (vb)[2]) - ((va)[2] * (vb)[1]); \
+  (vr)[1] = ((va)[2] * (vb)[0]) - ((va)[0] * (vb)[2]); \
+  (vr)[2] = ((va)[0] * (vb)[1]) - ((va)[1] * (vb)[0]); \
 }
 
 #endif

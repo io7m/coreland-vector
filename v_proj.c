622fd78b58301cc269bc6516f590bc0a6c1c21a0
m0 <m0@localhost>
2008-06-07 15:44:56 +0100

    cleanup v_proj.c

--- v_proj.c
+++ v_proj.c
@@ -9,45 +9,37 @@
 #include "v_multsc.h"
 
 /*
- * proj A = (A * B) / (||B|| ^ 2) * B
+ * project vb onto va.
  */
 
 float *
 vec_projNf (const float *va, const float *vb, float *vr, unsigned int n)
 {
   const float adp = vec_dotprodNf (va, vb, n);
-  float bms = vec_magNf (vb, n);
-
-  bms *= bms;
-  return vec_multscNfx (vb, vr, adp / bms, n);
+  const float bms = vec_magNf (vb, n);
+  return vec_multscNfx (vb, vr, adp / (bms * bms), n);
 }
 
 double *
 vec_projN (const double *va, const double *vb, double *vr, unsigned int n)
 {
   const double adp = vec_dotprodNd (va, vb, n);
-  float bms = vec_magNd (vb, n);
-
-  bms *= bms;
-  return vec_multscNdx (vb, vr, adp / bms, n);
+  const float bms = vec_magNd (vb, n);
+  return vec_multscNdx (vb, vr, adp / (bms * bms), n);
 }
 
 float *
 vec_projNf_aligned (const float *va, const float *vb, float *vr, unsigned int n)
 {
   const float adp = vec_dotprodNf_aligned (va, vb, n);
-  float bms = vec_magNf_aligned (vb, n);
-
-  bms *= bms;
-  return vec_multscNfx_aligned (vb, vr, adp / bms, n);
+  const float bms = vec_magNf_aligned (vb, n);
+  return vec_multscNfx_aligned (vb, vr, adp / (bms * bms), n);
 }
 
 double *
 vec_projN_aligned (const double *va, const double *vb, double *vr, unsigned int n)
 {
   const double adp = vec_dotprodNd_aligned (va, vb, n);
-  float bms = vec_magNd_aligned (vb, n);
-
-  bms *= bms;
-  return vec_multscNdx_aligned (vb, vr, adp / bms, n);
+  const float bms = vec_magNd_aligned (vb, n);
+  return vec_multscNdx_aligned (vb, vr, adp / (bms * bms), n);
 }
